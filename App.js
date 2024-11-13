09e1d9267b8452ab885bd3a4321ee0bb9a488fb0
Zhennyn <matheuslimamenezes2005@gmail.com>
2024-11-13 12:50:55 -0300

    Teste

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import styles from "./styles/Page.module.css";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export const Page = () => {
+	return (
+		<div className={styles.page1}>
+			<div className={styles.map}>Map</div>
+			<img className={styles.mindset1Icon} alt="" src="assets\mindset.png" />
+			<div className={styles.mind}>Mind</div>
+		</div>);
+};
